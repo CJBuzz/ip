0240a156e5612568c8ac2d3b1ba1d256468c4ef6
CJ <cj1000buzz@gmail.com>
2026-08-12 18:30:23 +0800

    Rename chatbot from Duke to Avon

---
+++ src/main/java/Avon.java
@@ -0,0 +1,11 @@
+public class Avon {
+    public static void main(String[] args) {
+        String banner = """
+                ___                    
+               /   |_   ______  ____  
+              / /| | | / / __ \\/ __ \\ 
+             / ___ | |/ / /_/ / / / / 
+            /_/  |_|___/\\____/_/ /_/ """;
+        System.out.println(banner);
+    }
+}
