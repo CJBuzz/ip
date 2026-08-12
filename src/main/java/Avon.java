3aa77b54022446282facf83693b8a089f9a49b67
CJ <cj1000buzz@gmail.com>
2026-08-12 18:40:14 +0800

    Add greet and exit

--- src/main/java/Avon.java
+++ src/main/java/Avon.java
@@ -1,11 +1,18 @@
 public class Avon {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = """
                 ___                    
                /   |_   ______  ____  
               / /| | | / / __ \\/ __ \\ 
              / ___ | |/ / /_/ / / / / 
             /_/  |_|___/\\____/_/ /_/ """;
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hark! I am Avon who stands before thee.");
+        System.out.println("How may my hand or wit now serve thy need?");
+        System.out.println(separator);
+        System.out.println("Fare thee well! Pray heavens our paths cross anon.");
+        System.out.println(separator);
     }
 }
